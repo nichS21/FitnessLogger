51aeacfed01d46997055779ab8e014755603b04b
mienpham04 <phammocmien04@gmail.com>
2025-04-25 11:17:51 -0400

    fix ui to show all courses

--- showCourse.php
+++ showCourse.php
@@ -20,22 +20,31 @@ $uid = 2;
     <title>AMNT Fitness Logger</title>
     <link rel="stylesheet" href="css/dashboard.css">
     <!-- <script src="script.js"></script> -->
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 
 <body>
     <?php
-        neededImports();
-        genNavBar();
+    neededImports();
+    genNavBar();
     ?>
     <main>
         <section class="courses">
             <h2><u>All courses:</u></h2>
             <div class="course-grid">
                 <?php
-                    genAllCourse($db);
+                genAllCourse($db);
                 ?>
             </div>
-            
+
+        </section>
+
+        <section class="actions">
+            <div class="buttons">
+                <button class="acctBtn mx-auto d-block" type="button" aria-expanded="false">
+                    <a href='/dashboard.php'>Back</a>
+                </button>
+            </div>
         </section>
     </main>
 </body>
