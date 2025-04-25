4d168ef35873d64232fa5e394f6782c846136077
nichS21 <nichstach@gmail.com>
2025-04-25 19:23:22 -0400

    MInor changes again.

--- scriptsPHP/util.php
+++ scriptsPHP/util.php
@@ -31,11 +31,11 @@ function neededImports()
 */
 function genNavBar()
 {
-?>
+?>      
     <div class="container-fluid site-color p-3 text-white"> 
         <div class="row">
             <div class="col-md-1">
-                <img src="images/amntLogo.png" height="50px" width="auto" class="mx-auto d-block" /> 
+                <img src="../images/amntLogo.png" height="50px" width="auto" class="mx-auto d-block" /> 
             </div>
             <div class="col-md-9">
                 <p class="fs-1" style="display:inline">AMNT Fitness Logger</p>
@@ -53,28 +53,6 @@ function genNavBar()
             </div>
         </div>
     </div>  
-        
-<div class="container-fluid site-color p-3 text-white"> 
-    <div class="row">
-        <div class="col-md-1">
-            <img src="../images/amntLogo.png" height="50px" width="auto" class="mx-auto d-block" /> 
-        </div>
-        <div class="col-md-9">
-            <p class="fs-1" style="display:inline">AMNT Fitness Logger</p>
-        </div>
-        <div class="col-md-2">
-            <div class="dropdown">
-                <button class="acctBtn mx-auto d-block dropdown-toggle" type="button" id="accountDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                    Account
-                </button>
-                <ul class="dropdown-menu acctColor" aria-labelledby="accountDropdown">
-                    <li><a class="dropdown-item acctColor" href="#">My Account</a></li>
-                    <li><a class="dropdown-item acctColor" href="#">Logout</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>  
 <?php
 }
 
