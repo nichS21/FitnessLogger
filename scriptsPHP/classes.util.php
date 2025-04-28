0e4d9407277bb3fca2062d7268c37f46012dccf4
akritinpl <139466603+akritinpl@users.noreply.github.com>
2025-04-27 23:11:53 -0400

    Feature: Courses need to have exactly 1 template

--- scriptsPHP/classes.util.php
+++ scriptsPHP/classes.util.php
@@ -106,3 +106,32 @@ function processLogin($db, $username, $password) {
     }
 
 }
+
+function showToast($message, $type = 'success') {
+    echo '
+    <div id="toast" class="toast ' . $type . '">' . $message . '</div>
+    <script>
+        setTimeout(function() {
+            const toast = document.getElementById("toast");
+            toast.classList.add("show");
+            setTimeout(() => toast.classList.remove("show"), 3000);
+        }, 1000);
+    </script>
+    ';
+}
+
+function createClass($db, $uid, $className, $classDes, $classLength, $imageUrl) {
+    try {
+        $sql = "INSERT INTO Course 
+                (uid, name, description, class_length, unsplash_url)
+                VALUES (?, ?, ?, ?, ?)";
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$uid, $className, $classDes, $classLength, $imageUrl]);
+
+        showToast("<strong>Course created successfully!</strong>", "success");
+    } catch (PDOException $e) {
+        showToast("Failed to create course", "error"); // . $e->getMessage();
+    }
+}
+?>
