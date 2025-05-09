6ca32a6947f23cd2be8ea9d50499c0cfdf84b181
mienpham04 <phammocmien04@gmail.com>
2025-05-08 20:32:00 -0400

    remove unnecessary try catch

--- enrollCourse.php
+++ enrollCourse.php
@@ -5,30 +5,15 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $courseID = $_POST['courseID'];
         $uid = $_POST['uid'];
+        
+        $stmt = $db->prepare("INSERT INTO Enrollment (uid, courseID) VALUES (?, ?)");
+        $stmt->execute([$uid, $courseID]);
 
-        if (!empty($courseID) && !empty($uid)) {
-            try {
-                $stmt = $db->prepare("INSERT INTO Enrollment (uid, courseID) VALUES (?, ?)");
-                $stmt->execute([$uid, $courseID]);
-
-                if (isset($_SESSION['available_courses'])) {
-                    $_SESSION['available_courses'] = array_filter($_SESSION['available_courses'], function ($course) use ($courseID) {
-                        return $course['courseID'] != $courseID;
-                    });
-                }
-                header("Location: showCourse.php");
-                exit;
-            } catch (PDOException $e) {
-                echo "<script>
-                alert('You might already be enrolled in this course.');
-                window.location.href = 'showCourse.php';
-            </script>";
-            }
-        } else {
-            echo "<script>
-            alert('Missing course or user ID');
-            window.location.href = 'showCourse.php';
-        </script>";
+        if (isset($_SESSION['available_courses'])) {
+            $_SESSION['available_courses'] = array_filter($_SESSION['available_courses'], function ($course) use ($courseID) {
+                return $course['courseID'] != $courseID;
+            });
         }
+        header("Location: showCourse.php");
     }
     ?>
